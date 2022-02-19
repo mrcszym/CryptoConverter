b0459f9805ae92c36b0dc818229e0f77c1cc1d8d
Szymon <marciniecszymon00@gmail.com>
2022-02-19 17:28:45 +0100

    added reverse button to change calculated currency multiple times

--- src/main/java/CryptoConvGUI.java
+++ src/main/java/CryptoConvGUI.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 
-public class CryptoConvGUI extends JFrame{
+public class CryptoConvGUI extends JFrame {
 
     private JPanel mainPanel;
     private JTextField passValueTextField;
@@ -15,9 +15,11 @@ public class CryptoConvGUI extends JFrame{
     private JLabel resultLabel;
     private JLabel oneBtcValueLabel;
     private JButton reverseBtn;
+    private JRadioButton btcRadio;
+    private JRadioButton ethereumRadio;
+    private JRadioButton tetherRadio;
 
     private double result;
-    private String calculatingCurrency = "toBTC";
 
     public CryptoConvGUI(String title) {
         super(title);
@@ -27,49 +29,98 @@ public class CryptoConvGUI extends JFrame{
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
-        this.setResizable(true);
+        this.setResizable(false);
         this.pack();
 
-        convertBtn.addActionListener(e -> {
+        oneBtcValueLabel.setText("Here will be real-time crypto currency value to PLN.");
 
-            if(calculatingCurrency.equals("toPLN")) {
-                result = parseAndCalculateResult(fetchBtcValue(), passValueTextField.getText(), calculatingCurrency);
-
-                if(result > 0.0) {
-                    resultLabel.setText(result + " PLN");
-                    oneBtcValueLabel.setText(fetchBtcValue() + " PLN = 1 BTC.");
-                } else {
-                    resultLabel.setText("Wrong data.");
-                }
-            } else if(calculatingCurrency.equals("toBTC")) {
-                result = parseAndCalculateResult(fetchBtcValue(), passValueTextField.getText(), calculatingCurrency);
-
-                if(result > 0.0) {
-                    resultLabel.setText(result + " BTC");
-                    oneBtcValueLabel.setText("1 BTC = " + fetchBtcValue() + " PLN.");
-                } else {
-                    resultLabel.setText("Wrong data.");
-                }
-            }
+        btcRadio.addActionListener(e -> {
+            passValueLabel.setText("PLN");
+            resultLabel.setText("BTC");
+            oneBtcValueLabel.setText("1 BTC = " + fetchBtcValue() + " PLN.");
 
         });
-        reverseBtn.addActionListener(e -> {
-            resultLabel.setText("Reversed!");
-            oneBtcValueLabel.setText("Enter your value and hit Convert button.");
+        ethereumRadio.addActionListener(e -> {
+            passValueLabel.setText("PLN");
+            resultLabel.setText("ETH");
+            oneBtcValueLabel.setText("1 ETH = " + fetchEthValue() + " PLN.");
 
-            if(calculatingCurrency.equals("toBTC")) {
-                passValueLabel.setText("BTC");
-                calculatingCurrency = "toPLN";
+        });
+        tetherRadio.addActionListener(e -> {
+            passValueLabel.setText("PLN");
+            resultLabel.setText("USDT");
+            oneBtcValueLabel.setText("1 USDT = " + fetchUsdtValue() + " PLN.");
+
+        });
+
+        convertBtn.addActionListener(e -> {
+            if (resultLabel.getText().length() > 4) // to erase "Result will show up here." and "Wrong value."
+                resultLabel.setText("BTC");
+            if (resultLabel.getText().equals("0"))
+                resultLabel.setText("0");
+
+            switch (passValueLabel.getText()) {
+                case "PLN" -> convertCalculatingCurrency();
+                case "BTC" -> {
+                    result = parseAndCalculateResult(fetchBtcValue(), passValueTextField.getText(), "PLN");
+                    resultLabel.setText(result + " PLN");
+                }
+                case "ETH" -> {
+                    result = parseAndCalculateResult(fetchEthValue(), passValueTextField.getText(), "PLN");
+                    resultLabel.setText(result + " PLN");
+                }
+                case "USDT" -> {
+                    result = parseAndCalculateResult(fetchUsdtValue(), passValueTextField.getText(), "PLN");
+                    resultLabel.setText(result + " PLN");
+                }
+                default -> System.out.println("Error at calculatingCurrency switch statement.");
             }
-            else if(calculatingCurrency.equals("toPLN")) {
-                passValueLabel.setText("BTC");
-                calculatingCurrency = "toBTC";
+        });
+
+        reverseBtn.addActionListener(e -> {
+            if (resultLabel.getText().length() > 4) { // to erase "Result will show up here." and "Wrong value."
+                resultLabel.setText("BTC");
             }
-            else System.out.println("Error at reverseBtn ActionListener.");
+            reverseCurrencies();
         });
     }
 
+    void reverseCurrencies() {
+        String tempCurrencyValue = passValueLabel.getText();
+        passValueLabel.setText(resultLabel.getText());
+        resultLabel.setText(tempCurrencyValue);
+        passValueTextField.setText("");
+    }
+
+    void convertCalculatingCurrency() {
+
+        switch (resultLabel.getText()) {
+            case "PLN" -> {
+                result = parseAndCalculateResult(fetchBtcValue(), passValueTextField.getText(), "PLN");
+                resultLabel.setText(result + " PLN");
+                oneBtcValueLabel.setText(fetchBtcValue() + " PLN = 1 BTC.");
+            }
+            case "BTC" -> {
+                result = parseAndCalculateResult(fetchBtcValue(), passValueTextField.getText(), "BTC");
+                resultLabel.setText(result + " BTC");
+                oneBtcValueLabel.setText(fetchBtcValue() + " PLN = 1 BTC.");
+            }
+            case "ETH" -> {
+                result = parseAndCalculateResult(fetchEthValue(), passValueTextField.getText(), "ETH");
+                resultLabel.setText(result + " ETH");
+                oneBtcValueLabel.setText(fetchEthValue() + " PLN = 1 ETH.");
+            }
+            case "USDT" -> {
+                result = parseAndCalculateResult(fetchUsdtValue(), passValueTextField.getText(), "USDT");
+                resultLabel.setText(result + " USDT");
+                oneBtcValueLabel.setText(fetchUsdtValue() + " PLN = 1 USDT.");
+            }
+            default -> System.out.println("Error at convertCalculatingCurrency() switch");
+        }
+    }
+
     public static void main(String[] args) {
+
         Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
         JFrame frame = new CryptoConvGUI("Crypto Currency Converter");
 
@@ -92,17 +143,46 @@ public class CryptoConvGUI extends JFrame{
         }
     }
 
+    static String fetchEthValue() {
+        try {
+            Document document = Jsoup.
+                    connect("https://www.google.com/search?q=1+ethereum+to+pln&client=opera-gx&sxsrf=APq-WBvl2QzFlq9r86qx-DJK2CsGmOchVg%3A1645260428750&ei=jK4QYqSbLerirgTx3KLoAg&oq=1+Ehterium+to+PLN&gs_lcp=Cgdnd3Mtd2l6EAEYADIJCAAQDRBGEIICOgcIABBHELADOgcIABCwAxBDOgYIABAHEB46BAgAEA06CAgAEA0QBRAeOggIABAIEA0QHkoECEEYAEoECEYYAFD3CVifHmCZLWgCcAF4AIAB0wGIAewGkgEFOC4wLjGYAQCgAQHIAQrAAQE&sclient=gws-wiz").get();
+            Elements elements = document.getElementsByClass("pclqee");
+
+            return elements.get(0).text();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    static String fetchUsdtValue() {
+        try {
+            Document document = Jsoup.
+                    connect("https://www.google.com/search?q=1+tether+to+pln&client=opera-gx&sxsrf=APq-WBsFRlZRTFDRwXKv7YthCyAKUPBMBg%3A1645260437431&ei=la4QYrPIGa2MrwS8pIrICg&ved=0ahUKEwjz5r3fsIv2AhUtxosKHTySAqkQ4dUDCA0&uact=5&oq=1+tether+to+pln&gs_lcp=Cgdnd3Mtd2l6EAMyBAgAEA06BwgAEEcQsAM6BwgAELADEEM6BggAEAcQHjoICAAQCBAHEB46CggAEAgQBxAKEB46CAgAEA0QBRAeOggIABAIEA0QHkoECEEYAEoECEYYAFD0B1jdDWCQEGgCcAF4AIABV4gB5wOSAQE2mAEAoAEByAEJwAEB&sclient=gws-wiz").get();
+            Elements elements = document.getElementsByClass("pclqee");
+
+            return elements.get(0).text();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
     double parseAndCalculateResult(String fetchedValue, String passedValue, String calculatedCurrency) {
         String fetchedValueComaModified = fetchedValue.replace(',', '.');
-        String finalFetchedValue = fetchedValueComaModified.replaceAll("\\s+",""); //no spaces
+        String finalFetchedValue = fetchedValueComaModified.replaceAll("\\s+", ""); //no spaces
 
         String passedValueComaModified = passedValue.replace(',', '.');
-        String finalPassedValue = passedValueComaModified.replaceAll("\\s+",""); //no spaces
+        String finalPassedValue = passedValueComaModified.replaceAll("\\s+", ""); //no spaces
 
-        if(calculatedCurrency.equals("toBTC"))
+        if (calculatedCurrency.equals("BTC") || calculatedCurrency.equals("ETH") || calculatedCurrency.equals("USDT"))
             return Double.parseDouble(finalPassedValue) / Double.parseDouble(finalFetchedValue);
-        else if(calculatedCurrency.equals("toPLN"))
+        else if (calculatedCurrency.equals("PLN"))
             return Double.parseDouble(finalPassedValue) * Double.parseDouble(finalFetchedValue);
         else return -1;
     }
+
 }
